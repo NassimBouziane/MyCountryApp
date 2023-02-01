1476828b96a4c35a5c2cdbe23a72dc99cbae610c
S.G <fomnen_s@etna-alternance.net>
2023-02-01 11:52:46 +0100

    [viewmodel] Instance Retrofit

---
+++ app/src/main/java/com/example/kotlin101/network/CountryApiService.kt
@@ -0,0 +1,35 @@
+package com.example.kotlin101.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL =
+    "https://restcountries.com/v3.1/"
+
+/**
+ * The Retrofit object with the Moshi converter.
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+/**
+ * A public interface that exposes the [getCountry] method
+ */
+interface CountryApiService {
+    /**
+    * Returns a [List] of [Country] and this method can be called from a Coroutine.
+    * The @GET annotation indicates that the "all" endpoint will be requested with the GET
+    * HTTP method
+    */
+    @GET("all")
+    suspend fun getCountry(): String
+}
+object CountryApi {
+    val retrofitService : CountryApiService by lazy {
+        retrofit.create(CountryApiService::class.java)
+    }
+}
