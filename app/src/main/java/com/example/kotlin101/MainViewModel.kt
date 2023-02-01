b76e0e4bf817a517bd5d907f56463e7270e0a212
nassimbouziane <bouzianenassim17102002@gmail.com>
2023-02-01 16:50:12 +0100

    [GSON] wip gson parsing

--- app/src/main/java/com/example/kotlin101/MainViewModel.kt
+++ app/src/main/java/com/example/kotlin101/MainViewModel.kt
@@ -1,19 +1,41 @@
 package com.example.kotlin101
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
-import com.github.kittinunf.result.getFailureOrNull
-import kotlinx.coroutines.runBlocking
-import java.net.HttpURLConnection
-import java.net.URL
-import kotlin.concurrent.thread
+import com.github.kittinunf.result.getOrNull
+import com.google.gson.Gson
+import kotlinx.coroutines.flow.MutableStateFlow
 
 
 class MainViewModel : ViewModel()  {
+    data class country (       val name: String? = null,
+                               var region: String? = null,
+                               var tld: String? = null,
+                               var cca2: String? = null,
+                               var ccn3: String? = null,
+                               var cca3: String? = null,
+                               var cioc: String? = null,
+                               var independent: String? = null,
+                               var status:String? = null,
+                               var unMember: String? = null,
+                               var currencies:String? = null,
+                               var idd: String? = null,
+                               var capital:String ? = null,
+                               var altSpellings:String? = null,
+                               var subregion:String? = null,
+                               var languages:String? = null,
+                               var translations:String? = null,
+                               var latlng :String? = null,
+                               var landlocked:String? = null,
+                               var area:String? = null,
+                               var flag:String? = null,
+                               var flags:String? = null,
+                               var demonyms:String? = null,
+                               var fra:String? = null)
+
+
+
 
 
       fun test() {
@@ -33,12 +55,20 @@ class MainViewModel : ViewModel()  {
 
                 when (result) {
                      is Result.Failure -> {
-                          val ex = result.getFailureOrNull()
+                          val ex = result.getOrNull()
                           println(ex)
                      }
                      is Result.Success -> {
                           val data = result.get()
                           println(data)
+                         val gson = Gson()
+                         val response = gson.fromJson(data, country::class.java)
+                         println(response)
+                         //val responseState = MutableStateFlow(emptyArray<country>())
+                         //responseState.value = response
+                         //println(responseState.value)
+
+
                      }
                 }
 
