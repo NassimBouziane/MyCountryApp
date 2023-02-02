f2ca49a55237bc8dd913709f198895a016500e7c
jordan hoarau <hoarau_j@etna-alternance.net>
2023-02-02 15:33:33 +0100

    ViewModel avec stateflow et retrofit pour les requettes API (pas sur que ca marche a 100%)

--- app/src/main/java/com/example/kotlin101/MainViewModel.kt
+++ app/src/main/java/com/example/kotlin101/MainViewModel.kt
@@ -1,14 +1,48 @@
-package com.example.kotlin101
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class MainViewModel : ViewModel() {
-    val paysName: LiveData
+@ExperimentalCoroutinesApi
+class CountriesViewModel : ViewModel() {
+    private val _states = MutableStateFlow(CountryState.Loading)
+    val states: StateFlow<CountryState>
+        get() = _states
+
     init {
-        val result = Repository.paysName
-        paysName = Transformations.map(result) { result -> result.value }
+        viewModelScope.launch {
+            try {
+                val countries = getCountries()
+                _states.value = CountryState.Success(countries)
+            } catch (e: Exception) {
+                _states.value = CountryState.Error(e.message ?: "Unknown error")
+            }
+        }
     }
-   }
+
+    private suspend fun getCountries(): List<Country> {
+        return withContext(Dispatchers.IO) {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://restcountries.com/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val api = retrofit.create(CountriesApi::class.java)
+            api.getAllCountries().await().map { it.name }
+        }
+    }
+}
+
+sealed class CountryState {
+    object Loading : CountryState()
+    data class Success(val countries: List<Country>) : CountryState()
+    data class Error(val message: String) : CountryState()
+}
+
+data class Country(val name: String)
+
+interface CountriesApi {
+    @GET("all")
+    fun getAllCountries(): Deferred<List<Country>>
 }
