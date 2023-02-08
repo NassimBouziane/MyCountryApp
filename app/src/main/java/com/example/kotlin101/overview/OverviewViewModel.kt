a019dd1ce9724928932a67c8492a9beb96a6f1d9
S.G <fomnen_s@etna-alternance.net>
2023-02-08 12:21:53 +0100

    [viewmodel] New Modification

--- app/src/main/java/com/example/kotlin101/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/kotlin101/overview/OverviewViewModel.kt
@@ -4,35 +4,32 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.ViewModel
 import com.example.kotlin101.network.CountryApi
 import kotlinx.coroutines.launch
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.flow.MutableStateFlow
+import com.example.kotlin101.network.Country
+import kotlinx.coroutines.flow.asStateFlow
 
 
 class MyViewModel: ViewModel() {
-    // The internal MutableLiveData that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
+    private val _country = MutableStateFlow<List<Country>>(emptyList())
+    val country = _country.asStateFlow()
 
-    // The external immutable LiveData for the request status
-    val status: LiveData<String> = _status
-
-    // Call getAllCountry() on init so we can display status immediately.
     init {
         getAllCountry()
     }
 
      /* Gets Countries  information from the country API Retrofit service and updates the
       [Countries] [List] [LiveData].*/
-
     private fun getAllCountry() {
         viewModelScope.launch {
             try {
-                val resquestResult = CountryApi.retrofitService.getCountry()
-                println("Success list of country retrieved")
-                println("Success: $resquestResult")
-                _status.value = "Success: $resquestResult list of country retrieved"
+                val requestResult = CountryApi.retrofitService.getCountry()
+                _country.value = requestResult
+                // println("Success list of country retrieved")
+                println("Success: $requestResult")
+                // _status.value = "Success: $requestResult list of country retrieved"
             } catch (e: Exception) {
                 println("Failure: ${e.message}")
-                _status.value = "Failure: ${e.message}"
+                // _status.value = "Failure: ${e.message}"
             }
         }
     }
